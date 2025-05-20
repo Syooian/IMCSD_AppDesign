727de4dd53d52c4786ebd86ba7df7fa7a23bd7e6
Syooian <msn0126@hotmail.com>
2025-05-20 10:34:55 +0800

    新的Layout View

--- app/src/main/java/com/example/testapplication1/MainActivity.java
+++ app/src/main/java/com/example/testapplication1/MainActivity.java
@@ -16,13 +16,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //EdgeToEdge.enable(this);//是否顯示上方的狀態列
+        setContentView(R.layout.new_layout);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 
     int ClickCount;
