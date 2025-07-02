309ba72efcb521bfb611349495397154cf9680ba
Syooian <msn0126@hotmail.com>
2025-07-02 16:27:28 +0800

    新增按鈕功能以導航至不同的佈局活動

--- app/src/main/java/com/example/testapplication1/my_Activity_Homework_MainLayout.java
+++ app/src/main/java/com/example/testapplication1/my_Activity_Homework_MainLayout.java
@@ -1,6 +1,8 @@
 package com.example.testapplication1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,5 +19,39 @@ public class my_Activity_Homework_MainLayout extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+
+        Button Btn_Constraint = findViewById(R.id.Btn_Constraint);
+        Btn_Constraint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoTo("my_Activity_Homework_ConstraintLayout");
+            }
+        });
+
+        Button Btn_Linear = findViewById(R.id.Btn_Linear);
+        Btn_Linear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoTo("my_Activity_Homework_LinearLayout");
+            }
+        });
+
+        Button Btn_Relative = findViewById(R.id.Btn_Relative);
+        Btn_Relative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoTo("my_Activity_Homework_RelativeLayout");
+            }
+        });
+    }
+
+
+    public void GoTo(String ActivityName) {
+        try {
+            Class<?> cls = Class.forName("com.example.testapplication1." + ActivityName);
+            startActivity(new android.content.Intent(this, cls));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
